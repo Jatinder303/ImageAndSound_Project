2495ece1386487fe21d0b25418c403e677f30346
Jatinder Singh <Jatinder303@gmail.com>
2020-11-05 11:01:56 +1300

    unit test

---
+++ Images_UnitTest_Project/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Images_UnitTest_Project")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Images_UnitTest_Project")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9460fb00-7737-4865-bd32-f78f85bfbd53")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Images_UnitTest_Project/UnitTest_Image.cs
@@ -0,0 +1,37 @@
+﻿
+using ImageAndSound_Project;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Images_UnitTest_Project
+{
+    [TestClass]
+    public class UnitTest_Image
+    {
+        LogicClass logic_obj = new LogicClass();
+        int unit_result;
+        [TestMethod]
+        public void Test_spin_neg1()
+        {
+            int spin_unit_data = logic_obj.spin();
+            Assert.AreNotEqual(0, spin_unit_data);
+        }
+
+        [TestMethod]
+        public void test_spin_neg2()
+        {
+            int spin_unit_data = logic_obj.spin();
+            Assert.AreNotEqual(7, spin_unit_data);
+        }
+
+        [TestMethod]
+        public void test_spin_posi()
+        {
+
+            int spin_unit_data = logic_obj.spin();
+            if (spin_unit_data >= 1 && spin_unit_data < 7)
+            {
+                unit_result = 1;
+            }
+            Assert.AreEqual(1, unit_result);
+        }
+    }
+}
